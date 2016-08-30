7c363ed70e4537fe1a40c8b69ec3f5f9d63e9228
root <root@6da46902b507>
2016-08-30 05:16:04 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,11 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "mobile",
+  "defaultLanguage" : "en",
+  "displayName" : "Mobi_30thaug",
+  "homePage" : "Main",
+  "name" : "Mobi_30thaug",
+  "platformType" : "MOBILE",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
